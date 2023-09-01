b007b3ba381832a0ca44d56fed47c2f750d3743e
TimAGX <ayomidetimothy28@gmail.com>
2023-09-01 11:06:30 +0100

    updated create account code

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const app = express();
 app.use(bodyParser.json());
 
 let accountCounter = 1;
-const accounts = {};
+let accounts = [];
 
 app.post("/accounts", (req, res) => {
-   
   const { accountHolder, dob, accountType, initialBalance } = req.body;
 
-  if (!accountHolder || !dob || !accountType || !initialBalance) {
+  if (
+    !accountHolder ||
+    !dob ||
+    !accountType ||
+    isNaN(parseInt(initialBalance))
+  ) {
     return res.status(400).json({
       error:
         "Account holder name, DOB, account type and initial balance are needed!",
@@ -19,7 +23,11 @@ app.post("/accounts", (req, res) => {
   }
   //request for new account information
 
-  const accountNumber = generateAccountNumber(accountHolder, accountType, initialBalance);
+  const accountNumber = generateAccountNumber(
+    accountHolder,
+    accountType,
+    initialBalance
+  );
   const newAccount = {
     accountNumber,
     accountHolder,
@@ -28,7 +36,10 @@ app.post("/accounts", (req, res) => {
     balance: initialBalance,
   };
 
-  accounts[accountNumber] = newAccount;
+  //   accounts[accountNumber] = newAccount;
+  console.log(newAccount);
+  accounts = [...accounts, newAccount];
+  console.log(accounts);
   accountCounter++;
 
   res.status(201).json(newAccount);
@@ -37,35 +48,39 @@ app.post("/accounts", (req, res) => {
 function generateAccountNumber(holder, type, balance) {
   const randomNumber = Math.floor(Math.random() * 1000000000); //9 digit number
   const formattedBalance = balance.toFixed(2).replace(".", ""); //remove decimals and format as string
-  const accountNumber = `${randomNumber}${accountCounter}${holder.substring(0, 3)}${type.substring(0, 2)}${formattedBalance}`;
+  const accountNumber = `${randomNumber}${accountCounter}${holder.substring(
+    0,
+    3
+  )}${type.substring(0, 2)}${formattedBalance}`;
 
-  return accountNumber.slice(0, 10)//ensuring it is a 10 digit number
+  return accountNumber.slice(0, 10); //ensuring it is a 10 digit number
 }
-
 
 //Resolve a Bank account
 
-app.get('/accounts/:accountNumber', (req,res) => {
-    const { accountNumber } = req.params;
-    const account = accounts[accountNumber];
-    
-    if(!account) {
-        return res.status(404).json({ error: 'Account not found.'});
-    }
-    
-    res.json(account);
-})
+app.get("/accounts/:accountNumber", (req, res) => {
+  const { accountNumber } = req.params;
+  // const account = accounts[accountNumber];
+  const account = accounts.filter((acc) => acc.accountNumber === accountNumber);
+  console.log("account found", account[0] ?? undefined);
+
+  if (!account) {
+    return res.status(404).json({ error: "Account not found." });
+  }
+
+  res.json(account);
+});
 
 // fetch all bank accounts
 
-app.get('/accounts', (req, res) => {
-    const accountList = Object.values(accounts);
-    res.json(accountList);
-    })
+app.get("/accounts", (req, res) => {
+  const accountList = Object.values(accounts);
+  res.json(accountList);
+});
 
-    //Server
+//Server
 
-    const PORT = 3000;
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
